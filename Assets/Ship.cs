16c5363bf7f439d3f1d5ba630f951e4eae5f4a56
ahmetegici <ahmetegici@gmail.com>
2024-11-30 17:05:18 +0300

    Movement
    Movement controls

---
+++ Assets/Ship.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Ship : MonoBehaviour
+{
+    float moveSpeed = 3;
+
+    bool moveUp;
+    bool moveDown;
+    bool moveRight;
+    bool moveLeft;
+    bool speedUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveUp = Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W);
+        moveDown = Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S);
+        moveLeft = Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A);
+        moveRight = Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D);
+        speedUp = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        float moveAmount = moveSpeed * Time.fixedDeltaTime;
+        if (speedUp)
+        {
+            moveAmount *=3;
+        }
+        Vector2 move = Vector2.zero;
+
+        if (moveUp)
+        {
+            move.y += moveAmount;
+        }
+
+        if (moveDown)
+        {
+            move.y -= moveAmount;
+        }
+
+        if (moveLeft)
+        {
+            move.x -= moveAmount;
+        }
+
+        if (moveRight)
+        {
+            move.x += moveAmount;
+        }
+
+        float moveMagnitude = Mathf.Sqrt(move.x * move.x + move.y * move.y);
+        if (moveMagnitude > moveAmount)
+        {
+            float ratio = moveAmount / moveMagnitude;
+            move *= ratio;
+        }
+
+
+        pos += move;
+
+        if (pos.x <= 1.5f)
+        {
+            pos.x = 1.5f;
+        }
+
+        if (pos.x >= 16f)
+        {
+            pos.x = 16f;
+        }
+
+        if (pos.y >= 9)
+        {
+            pos.y = 9;
+        }
+
+        if (pos.y <= 1)
+        {
+            pos.y = 1;
+        }
+
+   
+
+        transform.position = pos;
+    }
+
+}
